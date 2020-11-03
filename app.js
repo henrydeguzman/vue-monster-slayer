d47eecf1f3b8146fbf509effebf2f990d663b598
henrilics <henrydeguzman.java73@gmail.com>
2020-11-03 14:32:37 +0800

    Updating the health bars

--- app.js
+++ app.js
@@ -3,27 +3,36 @@ function getRandomValue (min, max) {
 }
 
 const app = Vue.createApp({
-     data () {
-          return {
-               monsterHealth: 100,
-               playerHealth: 100
-          }
-     },
-     methods: {
-          attackMonster () {
-               // if you attack, then the monster also attack
-               // Attack value between 12 to 5
-               const attackValue = getRandomValue(12, 5)
-               this.monsterHealth -= attackValue
-
-               // monster attack back
-               this.attackPlayer()
-          },
-          attackPlayer () {
-               // Attack value between 15 - 8
-               const attackValue = getRandomValue(15, 8);
-               this.playerHealth -= attackValue
-          }
-     }
-})
-app.mouth('#game')
+	data() {
+		return {
+			monsterHealth: 100,
+			playerHealth: 100,
+		};
+	},
+	computed: {
+		monsterBarStyles() {
+			return { width: this.monsterHealth + "%" };
+		},
+		playerBarStyles() {
+			return { width: this.playerHealth + "%" };
+		},
+	},
+	methods: {
+		attackMonster() {
+			// if you attack, then the monster also attack
+			// Attack value between 12 to 5
+			const attackValue = getRandomValue(12, 5);
+			this.monsterHealth -= attackValue;
+			console.log("monster: " + this.playerHealth);
+			// monster attack back
+			this.attackPlayer();
+		},
+		attackPlayer() {
+			// Attack value between 15 - 8
+			const attackValue = getRandomValue(15, 8);
+			this.playerHealth -= attackValue;
+			console.log("player: " + this.playerHealth);
+		},
+	},
+});
+app.mount('#game')

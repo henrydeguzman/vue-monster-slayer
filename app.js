b1efaa6da6f58cdb044a659aeb1f42150444f921
henrilics <henrydeguzman.java73@gmail.com>
2020-11-03 14:09:41 +0800

    Project setup and first methods

---
+++ app.js
@@ -0,0 +1,29 @@
+function getRandomValue (min, max) {
+     return Math.floor(Math.random() * (max - min)) + min
+}
+
+const app = Vue.createApp({
+     data () {
+          return {
+               monsterHealth: 100,
+               playerHealth: 100
+          }
+     },
+     methods: {
+          attackMonster () {
+               // if you attack, then the monster also attack
+               // Attack value between 12 to 5
+               const attackValue = getRandomValue(12, 5)
+               this.monsterHealth -= attackValue
+
+               // monster attack back
+               this.attackPlayer()
+          },
+          attackPlayer () {
+               // Attack value between 15 - 8
+               const attackValue = getRandomValue(15, 8);
+               this.playerHealth -= attackValue
+          }
+     }
+})
+app.mouth('#game')

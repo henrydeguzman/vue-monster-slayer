f44e7bef06946c72da5d5de97e01b852e747f728
henrilics <henrydeguzman.java73@gmail.com>
2020-11-03 16:10:32 +0800

    Adding functionalities

--- app.js
+++ app.js
@@ -6,21 +6,33 @@ const app = Vue.createApp({
 	data() {
 		return {
 			monsterHealth: 100,
-			playerHealth: 100,
+               playerHealth: 100,
+               currentRound: 0
 		};
-	},
+     },
+     watch: {
+          currentRound (value, oldval) {
+               if (value >= 3) {
+
+               }
+          }
+     },
 	computed: {
 		monsterBarStyles() {
 			return { width: this.monsterHealth + "%" };
 		},
 		playerBarStyles() {
 			return { width: this.playerHealth + "%" };
-		},
+          },
+          divisible () {
+               return this.currentRound % 3 !== 0
+          }
 	},
 	methods: {
 		attackMonster() {
 			// if you attack, then the monster also attack
-			// Attack value between 12 to 5
+               // Attack value between 12 to 5
+               this.currentRound++
 			const attackValue = getRandomValue(12, 5);
 			this.monsterHealth -= attackValue;
 			console.log("monster: " + this.playerHealth);
@@ -33,6 +45,22 @@ const app = Vue.createApp({
 			this.playerHealth -= attackValue;
 			console.log("player: " + this.playerHealth);
 		},
+		specialAttackMonster() {
+               this.currentRound++
+			const attackValue = getRandomValue(10, 25);
+			this.monsterHealth -= attackValue;
+			this.attackPlayer();
+          },
+          healPlayer () {
+               this.currentRound++
+               const healValue = getRandomValue(8, 20)
+               if (this.playerHealth + healValue > 100) {
+                    this.playerHealth = 100
+               } else {
+                    this.playerHealth += healValue
+               }        
+               this.attackPlayer()       
+          }
 	},
 });
 app.mount('#game')
